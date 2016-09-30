7dc4988d4572125274f8fc5b18144900988f9f09
Temirulan <mtemirulan@gmail.com>
2016-09-30 15:53:51 +0600

    Add files via upload

--- even_numbers.cpp
+++ even_numbers.cpp
@@ -7,12 +7,13 @@ int main () {
 	int n, a[101];
 
 	cin >> n;
+
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
 
 	for (int i = 1; i <= n; i++) {
-		if (i % 2 == 1) {
+		if (a[i] % 2 == 0) {
 			cout << a[i] << " ";
 		}
 	}
--- reverse.cpp
+++ reverse.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main () {
 
-	int n, a[101];
+	int n, a[30];
 
 	cin >> n;
+
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
+	// a[1] a[2] ... a[n]
 
-	for (int i = 1; i <= n; i++) {
-		if (i % 2 == 1) {
-			cout << a[i] << " ";
-		}
+	for (int i = n; i >= 1; i--) {
+		cout << a[i] << " ";
 	}
 	cout << endl;
 
